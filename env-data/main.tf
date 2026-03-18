75db95f364697a988b51e4fd503d155997836f7a
Sang-un <dltkddns79@gmail.com>
2026-03-18 16:32:44 +0900

    aca env create

---
+++ env-data/main.tf
@@ -0,0 +1,27 @@
+module "aca_app" {
+  source = "../modules/aca-app"
+
+  # 환경 설정
+  env                = "dev"
+  create_environment = true
+  cae_name           = "acaenv-krc-dev-data"
+  cae_rg_name        = "rg-krc-dev-data"
+  
+  # 네트워크 (선택)
+  infrastructure_subnet_id       = "/subscriptions/a98144a1-06aa-4136-9f70-d68d15be60f3/resourceGroups/rg-krc-dev-common/providers/Microsoft.Network/virtualNetworks/vnet-krc-dev-common/subnets/snet-krc-dev-data-aca"
+  internal_load_balancer_enabled = true
+  zone_redundancy_enabled        = true
+
+  # 앱 설정
+  app_name           = "temp-app-data"
+  acr_server         = "" # MCR 사용 시 빈 문자열
+  image              = "mcr.microsoft.com/k8se/quickstart:latest"
+  cpu                = 0.25
+  memory             = "0.5Gi"
+  target_port        = 80
+
+  # Secret & 민감 정보 (전달)
+  uami_client_id     = var.uami_client_id
+  subscription_id    = var.subscription_id
+  uami_resource_id   = "/subscriptions/a98144a1-06aa-4136-9f70-d68d15be60f3/resourceGroups/rg-krc-dev-common/providers/Microsoft.ManagedIdentity/userAssignedIdentities/uami-krc-dev-acrpull"
+}
