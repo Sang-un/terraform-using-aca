5d3cf78b02d02b1cb68cac373f2b9c9e9be246e6
Sang-un <dltkddns79@gmail.com>
2026-03-18 17:23:23 +0900

    pointail

---
+++ env-pointail/main.tf
@@ -0,0 +1,27 @@
+module "aca_app" {
+  source = "../modules/aca-app"
+
+  # 환경 설정
+  env                = "dev"
+  region             = "jpe"
+  project            = "pointail"
+  create_environment = true
+  
+  # 네트워크 (선택)
+  infrastructure_subnet_id       = "/subscriptions/a98144a1-06aa-4136-9f70-d68d15be60f3/resourceGroups/rg-jpe-dev-common/providers/Microsoft.Network/virtualNetworks/vnet-jpe-dev-common/subnets/snet-jpe-dev-pointail-aca"
+  internal_load_balancer_enabled = true
+  zone_redundancy_enabled        = true
+
+  # 앱 설정
+  app_name           = "temp-app-pointail"
+  acr_server         = "" # MCR 사용 시 빈 문자열
+  image              = "mcr.microsoft.com/k8se/quickstart:latest"
+  cpu                = 0.25
+  memory             = "0.5Gi"
+  target_port        = 80
+
+  # Secret & 민감 정보 (전달)
+  uami_client_id     = var.uami_client_id
+  subscription_id    = var.subscription_id
+  uami_resource_id   = "/subscriptions/a98144a1-06aa-4136-9f70-d68d15be60f3/resourceGroups/rg-jpe-dev-common/providers/Microsoft.ManagedIdentity/userAssignedIdentities/uami-jpe-dev-acrpull"
+}
